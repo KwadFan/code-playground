97e4c215064ccfa8d96df514c4316c5a4f4c68be
Stephan Wendel <me@stephanwe.de>
2023-02-08 18:24:12 +0100

    chore: what I learned...
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

--- ground-c/C-Files/HelloWorld.c
+++ ground-c/C-Files/HelloWorld.c
@@ -6,12 +6,20 @@ int main(){
 
     // Single line comment
 
-/*
+    /*
     Multi
     line
     comment
-*/
+    */
+    printf("HelloWorld"); // Prints no new line after string
+    printf("Hello World\n"); // Prints new line after string
+    printf("This\tline\thas\ttabs!\n"); // Print line with tabs
 
-    printf("Hello World\n");
+    /*
+        A backslach is used as escape sequence
+    */
+
+    printf("Show how to print special characters like Quotes \" \'");
+    printf("What if I have to show a Backslash \\ ?"); // Print a backslash needs to be escaped..
     return 0;
 }
