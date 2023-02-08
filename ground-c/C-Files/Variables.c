87061b0c858283ec48f754ea5c99113761d5a79f
Stephan Wendel <me@stephanwe.de>
2023-02-08 18:41:51 +0100

    chore: add variables
    Yay, learned basic datatypes and variables
    
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

---
+++ ground-c/C-Files/Variables.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+
+    // variable = Allocated space in Memory to store a value.
+
+    int x; // declaration of an integer variable
+    x = 123; // initialization
+
+    int y = 321; // combined declaration and initialization
+
+    // Basic Data types
+    int age = 21; // integer
+    float gpa = 2.05; // floating point number
+    char grade = 'C'; // single character
+
+    /*
+        Quote: C is not an Object Oriented Language, so there is no
+        string data type, strings are technically objects...
+
+        To store a string we need an Array to store more than one character..
+    */
+    char name[] = "Bro"; // array of characters
+
+    return 0;
+}
