99058a20b32d452389e56d2a0cfde613f4dfebee
Stephan Wendel <me@stephanwe.de>
2023-11-18 23:42:03 +0100

    chore: concatenate two json files into json array
    Signed-off-by: Stephan Wendel <me@stephanwe.de>

---
+++ ground-py/rpi-imager-json/concat-json.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+'''
+    Concatenate two json files and create 'os-list' JSON array.
+    write to file rpi-imager.json file to finish task
+'''
+
+# Since we dont know how many files are there, lets determine first
+
+import os
+import json
+
+# init empty file list
+json_files = []
+
+# Init os-list
+os_list = []
+
+# init data dict
+data = {}
+
+for file in os.listdir(os.getcwd()):
+    if file.endswith('.json'):
+        file = os.path.join(os.getcwd(), file)
+        json_files.append(file)
+# sort file list
+json_files.sort()
+
+# write json to dict
+for json_file in json_files:
+    with open(json_file, 'r') as file:
+        json_string = json.load(file)
+        os_list.append(json_string)
+
+# convert dict to json
+data = {"os-list": os_list}
+data = json.dumps(data)
+
+with open('./rpi-imager.json', 'w') as rpi_json_file:
+    rpi_json_file.write(data)
